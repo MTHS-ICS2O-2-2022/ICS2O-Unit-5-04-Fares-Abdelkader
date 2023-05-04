7429887dd3a93e3b55b784a6b30cbfe232b07036
Fares-Abdelkader <124618781+Fares-Abdelkader@users.noreply.github.com>
2023-05-04 16:37:43 +0000

    style.css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fares.Abdelkader
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+
+'use strict'
+
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-UNIT-5-04-HTML/sw.js", {
+    scope: "/ICS2O-UNIT-5-04-HTML/",
+  })
+}
+
+/**
+ * Input.
+ */
+
+function buttonClicked() {
+
+  const age = parseInt(document.getElementById('age').value)
+
+  var monday = document.getElementById("monday")
+  var tuesday = document.getElementById("tuesday")
+  var wednesday = document.getElementById("wednesday")
+  var thursday = document.getElementById("thursday")
+  var friday = document.getElementById("friday")
+  var sunday = document.getElementById("sunday")
+  var saturday = document.getElementById("saturday")
+
+  if ((tuesday.checked == true || thursday.checked == true) || (age >= 12 && age <= 21)) {
+    document.getElementById("output").innerHTML =
+      "You are eligible for student pricing!"
+    console.log("eligible for student pricing")
+  } else {
+    document.getElementById("output").innerHTML =
+      "You must pay regular pricing."
+    console.log("not eligible for student pricing")
+  }
+}
